84cbf826b1639c118810b6236e055629d50fc223
CarolGitonga <carolgitongaofficial@gmail.com>
2024-10-14 11:06:13 +0300

    create property model

---
+++ property/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.urls import reverse_lazy
+from django.utils import timezone
+
+
+# Create your models here.
+class Property(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField(verbose_name='Property Price')
+    description = models.TextField (verbose_name='Property Details')
+    location = models.CharField (max_length=200,verbose_name='Property Location')
+    date_added = models.DateField(default=timezone.now, verbose_name='Date Registered')
+
+    class Meta:
+        verbose_name_plural = 'Properties'
+        ordering = ['name', 'price']
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse_lazy('property:detail', kwargs={'pk:self.pk'})
+    
